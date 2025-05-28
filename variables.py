15f6428391d650758f204c61591608054f3ee917
DalmaSolda <dalmasolda560@gmail.com>
2025-05-28 18:02:15 -0300

    Clase 1 - Variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+#Numeros enteros
+num1 = 10 
+
+#Numeros flotantes
+flo1 = 10.5 
+
+#String
+cadena = "Hola, Python!"
+
+#Booleanos 
+booleano = True 
+
+print (type(numero))
+numero = int(input("Ingrese un numero:"))
+  
